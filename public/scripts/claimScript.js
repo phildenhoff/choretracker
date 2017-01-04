91ef56ccfb442cf0b2bc6b659d196afae8e53dc4
phildenhoff <phildenhoff@gmail.com>
2017-01-04 12:56:40 -0800

    Confirm js scripts added

---
+++ public/scripts/claimScript.js
@@ -0,0 +1,84 @@
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "claimLoad|newClaim|submitClaim|getCookie" }] */
+/* global socket */
+/* eslint-env browser */
+
+var taskData = {}
+
+function claimLoad () {
+  // load all taskData into the options
+
+  taskData = JSON.parse(localStorage.taskData)
+  // clear options
+  document.getElementById('selectClaim').innerHTML = ''
+  // then load all taskData into the options
+  for (const taskID in taskData) {
+    var option = '"<option value="' + taskID + '">' + taskData[taskID][1] + '</option>'
+    // taskData[taskID][1] is the pretty version of the task
+    document.getElementById('selectClaim').innerHTML += option
+  }
+}
+
+function newClaim () {
+  // loads text into HTML elements
+  var e = document.getElementById('selectClaim')
+
+  // prepare to print selected text to user again
+  var text = e.options[e.selectedIndex].text
+  text = text[0].toLowerCase() + text.slice(1)
+
+  var pointsWorth = taskData[e.value][0]
+
+  document.getElementById('chosenAction').innerHTML = text
+  document.getElementById('pointsWorth').innerHTML = pointsWorth
+
+  // when user presses "Done" button to claim points,
+  // should post selected action, user, and authentication
+  // to server. Server then determines point worth, adds to total,
+  // adds action to queue.
+}
+
+function submitClaim () {
+  // submit claim to server
+
+  let username
+  var name = 'username='
+  var ca = document.cookie.split(';') // cookie
+  var taskID = document.getElementById('selectClaim').value
+
+  // get username to submit
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i]
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) === 0) {
+      username = c.substring(name.length, c.length)
+    }
+  }
+
+  socket.emit('claim', ('username:' + username + ';taskID:' + taskID))
+  // now that claim is submitted, need to save data for claimed.html
+
+  // saves task ID and worth
+  var pointWorth = taskData[taskID][0]
+  localStorage.lastClaim = taskID + ';' + pointWorth
+  // if user total score DNE, set to 0 then add what user just got
+  if (localStorage.userTotalScore == null) localStorage.userTotalScore = 0
+  localStorage.userTotalScore = Number(localStorage.userTotalScore) + Number(pointWorth)
+  window.location.href = '/public/claimed.html'
+}
+
+function getCookie (cname) {
+  var name = cname + '='
+  var ca = document.cookie.split(';')
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i]
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ''
+}
---
+++ public/scripts/main.js
@@ -0,0 +1,120 @@
+/* eslint no-unused-vars: ['error', { 'varsIgnorePattern': 'load|signOut|claimed' }] */
+/* global socket */
+/* eslint-env browser */
+
+function load () {
+  var currentUser = signIn(function (username) {
+    // if user is on scoreboard, set them as selected
+    // in the future, this should read from userScores or something.
+    if (username === 'phil' || username === 'ross' || username === 'karl') {
+      document.getElementById(username).className += ' selected'
+    } else if (username === 'philip') {
+      username = username.substring(0, 4)
+      document.getElementById(username).className += ' selected'
+    } else {
+      // console.log('User not on scoreboard, displaying alt score')
+      document.getElementById('alt').className += ' selected'
+    }
+    socket.emit('auth', username)
+  })
+
+  socket.emit('taskData')
+
+  socket.on('taskList', function (tasks) {
+    // console.log('Received task list from server')
+    localStorage.setItem('taskData', JSON.stringify(tasks))
+  })
+
+  socket.on('scoreUpdate', function (scores) {
+    // update score for each user on scoreboard
+    var currentUser = getCookie('username')
+    // as a backup, ALWAYS set the alt scoreboard to a score of 0.
+    document.getElementById('alt').innerHTML = currentUser.charAt(0).toUpperCase() + currentUser.slice(1) + ': 0 &#x1F31F'
+
+    for (const user in scores) {
+      var capitalisedUser = user.charAt(0).toUpperCase() + user.slice(1)
+      // if user is current user, also update their userTotalScore
+      // and set the alt display to their data
+      if (user === currentUser) {
+        // console.log('Score update. Updating current user\'s score and setting alt info to their data.')
+        capitalisedUser = currentUser.charAt(0).toUpperCase() + currentUser.slice(1)
+        localStorage.userTotalScore = scores[user]
+        document.getElementById('alt').innerHTML = capitalisedUser + ': ' + scores[user] + ' &#x1F31F'
+        // Hailey mode below
+        if (currentUser === 'philip') document.getElementById('phil').innerHTML = capitalisedUser + ': ' + scores[user] + ' &#x1F31F'
+      }
+      // if the element is one of the score board, it needs to be updated
+      let element = document.getElementById(user.toLowerCase())
+      if (element) {
+        element.innerHTML = (capitalisedUser + ': ' + scores[user] + ' &#x1F31F')
+      }
+    }
+  })
+
+  socket.on('highestScore', function (data) {
+    let username = data[0]
+    let score = data[1]
+    if (document.getElementById('highscoreUsername')) {
+      document.getElementById('highscoreUsername').innerHTML = username.charAt(0).toUpperCase() + username.slice(1)
+      document.getElementById('highscorePoints').innerHTML = score
+    }
+  })
+}
+
+function signIn (callback) {
+  // read username cookie
+  // call backs make this fully asynch (weee js!)
+  var username = getCookie('username')
+  if (username == null || username === '') {
+    while (username === '' || username == null) {
+      username = prompt('Please enter your name ')
+      username = username.toLowerCase()
+      setCookie('username', username, 1)
+      callback(username)
+    }
+  } else {
+    username = getCookie('username')
+    callback(username)
+  }
+}
+
+function signOut () {
+  setCookie('username', '')
+  location.reload()
+}
+
+function claimed () {
+  // user claimed points on last page. Here, update with live view of how many
+  // points added, and what user running total is
+  var pointsWorth = Number(localStorage.lastClaim.split(';')[1])
+  document.getElementById('pointsWorth').innerHTML = pointsWorth
+  document.getElementById('userTotalScore').innerHTML = Number(localStorage.userTotalScore)
+  if (pointsWorth === 1) document.getElementById('gainedPointsPlural').innerHTML = ''
+  pointsWorth = null
+}
+
+/*
+ * All that cookie business is beyond here
+ */
+
+function getCookie (cname) {
+  var name = cname + '='
+  var ca = document.cookie.split(';')
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i]
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1)
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ''
+}
+
+function setCookie (cname, cvalue, exdays) {
+  var d = new Date()
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
+  var expires = 'expires=' + d.toUTCString()
+  document.cookie = cname + '=' + cvalue + ';' + expires + 'path=/'
+}
